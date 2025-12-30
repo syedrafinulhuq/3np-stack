28d43425042d187249e55c8bb4d9d34f8275bb63
syedrafinulhuq <syedrafinulhuq@gmail.com>
2025-12-30 22:23:38 +0600

    Initial commit

--- nest-auth/src/app.controller.ts
+++ nest-auth/src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('status')
+  getStatus() {
+    return { ok: true, service: 'NestJS API', version: '1.0.0' };
   }
 }
--- nest-auth/src/main.ts
+++ nest-auth/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api'); // so routes start with /api
+
+  await app.listen(3000);
+  console.log(`Server running on http://localhost:3000/api`);
 }
 bootstrap();
